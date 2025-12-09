0bdc2e46e9736b95d31c949c3cdb84a4117d17e4
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-09 23:15:33 +0100

    chore: play with friends

--- frontend/components/guest/HeroSection.tsx
+++ frontend/components/guest/HeroSection.tsx
@@ -327,7 +327,7 @@ const HeroSection: React.FC = () => {
                 </svg>
                 <span className="absolute inset-0 flex items-center justify-center text-[#00F0FF] capitalize text-[12px] font-dmSans font-medium z-2">
                   <Gamepad2 className="mr-1.5 w-[16px] h-[16px]" />
-                  Create Game
+                 Play with Friends
                 </span>
               </button>
 
