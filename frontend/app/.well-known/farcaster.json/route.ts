f8bf03755c3a67909b19ddeb9c64755e81d182c7
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-12 19:19:00 +0100

    chore: launched mini app

--- frontend/app/.well-known/farcaster.json/route.ts
+++ frontend/app/.well-known/farcaster.json/route.ts
@@ -1,13 +1,9 @@
-import { minikitConfig } from "../../../minikit.config";
-
-function withValidProperties(properties: Record<string, undefined | string | string[]>) {
-return Object.fromEntries(
-    Object.entries(properties).filter(([_, value]) => (Array.isArray(value) ? value.length > 0 : !!value))
-);
-}
+import { NextResponse } from "next/server";
 
 export async function GET() {
-// const URL = process.env.NEXT_PUBLIC_URL as string;
-const URL = "https://base-monopoly.vercel.app/" as string;
-return Response.json(minikitConfig); // see the next step for the manifest_json_object
+  const farcasterHostedManifest =
+    "https://api.farcaster.xyz/miniapps/hosted-manifest/019b13c3-d4cd-eaac-a018-d241e391fe0c";
+
+  // Send a 307 Temporary Redirect
+  return NextResponse.redirect(farcasterHostedManifest, 307);
 }
