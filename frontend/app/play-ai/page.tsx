91a681cd034d6222fd5f96087108921a544f8c37
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-12 14:24:37 +0100

    chore: fixing mobile responsiveness

--- frontend/app/play-ai/page.tsx
+++ frontend/app/play-ai/page.tsx
@@ -1,13 +1,9 @@
-import { useMediaQuery } from "@/components/useMediaQuery";
 import PlayWithAISettings from "@/components/settings/game-ai";
-import PlayWithAIMobile from "@/components/settings/game-aimobile";
 
 export default function GameSettingsPage() {
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
-  return (
-    <main className="w-full overflow-x-hidden">
-      {isMobile ? <PlayWithAIMobile /> : <PlayWithAISettings />}
-    </main>
-  );
+    return (
+        <main className="w-full overflow-x-hidden">
+            <PlayWithAISettings />
+        </main>
+    );
 }
