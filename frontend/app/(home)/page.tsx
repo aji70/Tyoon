8630e4c20933f2746503c1d9ebdcd5737a5016f5
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-14 17:16:32 +0100

    feat: fixed mobile nav bar

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,14 +1,17 @@
 import HeroSection from "@/components/guest/HeroSection";
+import HeroSectionMobile from "@/components/guest/HeroSection-mobile";
 import HowItWorks from "@/components/guest/HowItWorks";
 import JoinOurCommunity from "@/components/guest/JoinOurCommunity";
 import WhatIsTycoon from "@/components/guest/WhatIsTycoon";
 import Footer from "@/components/shared/Footer";
+import { useMediaQuery } from "@/components/useMediaQuery";
 
 
 export default function Home() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
     <main className="w-full">
-      <HeroSection />
+       {isMobile ? <HeroSectionMobile /> : <HeroSection />}
       <WhatIsTycoon />
       <HowItWorks />
       <JoinOurCommunity />
