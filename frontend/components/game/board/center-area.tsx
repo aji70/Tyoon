6ad7175ae404412fec02c8eb4653cf2ae470d082
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-28 19:52:41 +0100

    feat: fixing bankruptcy using wallet connect

--- frontend/components/game/board/center-area.tsx
+++ frontend/components/game/board/center-area.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import DiceAnimation from "./dice-animation";
-import RollResult from "../roll-result";
+import RollResult from "./roll-result";
 import ActionLog from "../action-log";
 
 import { Property, Player, Game } from "@/types/game";
