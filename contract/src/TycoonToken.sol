802b34764861cd42cc2f6297296ce135d0a8c815
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-21 19:21:25 +0100

    update

--- contract/src/TycoonToken.sol
+++ contract/src/TycoonToken.sol
@@ -6,7 +6,7 @@ import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol"
 import {ERC20Burnable} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Token is ERC20, ERC20Burnable, Ownable {
+contract TYC is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner) ERC20("Tycoon", "TYC") Ownable(initialOwner) {
         _mint(initialOwner, 1000000000000000000000000);
     }
